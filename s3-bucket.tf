adaaaa9d4a4a0de3a120ce300aa78b8d26c95664
Jose Rangel <jose.rangel@mindcurv.com>
2022-04-20 10:37:39 +0200

    commenting out s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,25 +1,25 @@
-resource "aws_s3_bucket" "s3Bucket" {
-  bucket = "jrangelpersonal-sonarcloud"
-  acl    = "public-read"
+# resource "aws_s3_bucket" "s3Bucket" {
+#   bucket = "jrangelpersonal-sonarcloud"
+#   acl    = "public-read"
 
-  policy = <<EOF
-{
-     "id" : "MakePublic",
-   "version" : "2012-10-17",
-   "statement" : [
-      {
-         "action" : [
-             "s3:GetObject"
-          ],
-         "effect" : "Allow",
-         "resource" : "arn:aws:s3:::jrangelpersonal-sonarcloud/*",
-         "principal" : "*"
-      }
-    ]
-  }
-EOF
+#   policy = <<EOF
+# {
+#      "id" : "MakePublic",
+#    "version" : "2012-10-17",
+#    "statement" : [
+#       {
+#          "action" : [
+#              "s3:GetObject"
+#           ],
+#          "effect" : "Allow",
+#          "resource" : "arn:aws:s3:::jrangelpersonal-sonarcloud/*",
+#          "principal" : "*"
+#       }
+#     ]
+#   }
+# EOF
 
-  /* website {
-    index_document = "index.html"
-  } */
-}
+#   /* website {
+#     index_document = "index.html"
+#   } */
+# }
